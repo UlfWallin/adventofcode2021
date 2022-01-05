76aa176ad109e89096ddaebfa849a7cd6c083be2
UlfWallin <herr.wallin@gmail.com>
2022-01-05 17:08:37 +0100

    Part One complete

--- day-4/part_one.py
+++ day-4/part_one.py
@@ -1,25 +1,26 @@
 import numpy as np
+from pprint import pprint
+import logging
 
-draws = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
-boards = [
-    [22, 13, 17, 11,  0,
-     8,  2, 23,  4, 24,
-    21,  9, 14, 16,  7,
-     6, 10,  3, 18,  5,
-     1, 12, 20, 15, 19],
+draws = []
+boards = []
 
-    [ 3, 15,  0,  2, 22,
-     9, 18, 13, 17,  5,
-    19,  8,  7, 25, 23,
-    20, 11, 10, 24,  4,
-    14, 21, 16, 12,  6],
+with open('input/input.txt') as file:
+    header = file.readline() # Read draws
+    draws.extend(map(int, header.split(',')))
+    file.readline() # Skip empty line
+    nums = []
+    for line in file:
+        nums.extend(map(int, line.split()))
+        if line == '\n':
+            boards.append(nums)
+            nums = []
+    else:
+        boards.append(nums)
 
-    [14, 21, 17, 24,  4,
-    10, 16, 15,  9, 19,
-    18,  8, 23, 26, 20,
-    22, 11, 13,  6,  5,
-     2,  0, 12,  3,  7]
-]
+print(draws)
+print(boards)
+
 
 bingo = False
 bingo_board = 0
@@ -45,5 +46,6 @@ while not bingo:
     
 
 unmarked = np.setdiff1d(boards[board_index], draws[:number_of_draws])
-print(unmarked)
-print(sum(unmarked))
+sum_unmarked = sum(unmarked)
+last_called = draws[number_of_draws-1]
+print(f'Sum unmarked = {sum_unmarked}, Last Called = {last_called}. {sum_unmarked} * {last_called} = {sum_unmarked * last_called}')
