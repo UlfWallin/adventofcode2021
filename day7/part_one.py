f3c44469ac49257d09e2c12b9a2b715e1b9e07ca
UlfWallin <herr.wallin@gmail.com>
2022-01-10 21:17:22 +0100

    Readability

--- day7/part_one.py
+++ day7/part_one.py
@@ -3,7 +3,6 @@ import numpy as np
 crab_pos = np.fromfile("input/input.txt", sep=',')
 
 m = np.median(crab_pos)
-cost = abs(crab_pos - m)
+cost = abs(crab_pos - m).sum()
 
-print(m)
-print(cost.sum())
+print(f"Move to {m}. Total cost: {cost}")
